04c707ea7785fe8c63cbf64ca058dc12551ae144
Ron Pedde <ron@pedde.com>
2007-04-20 04:15:45 +0000

    Merge r1501 to r1515 into roku branch

--- win32/FireflyShell/resource.h
+++ win32/FireflyShell/resource.h
@@ -46,6 +46,8 @@
 #define IDC_AUTOSTART                   1012
 #define IDC_WEBADMIN                    1013
 #define IDC_WEBSITE                     1014
+#define IDC_AUTOSTART2                  1014
+#define IDC_AUTOSTART_ICON              1014
 #define IDC_VERSIONLIST                 1015
 #define IDC_COPY                        1016
 #define IDC_REFRESH                     1018
@@ -53,6 +55,7 @@
 #define IDC_FIREFLYLINK                 1020
 #define IDC_FIREFLYLINK2                1021
 #define IDC_ROKULINK                    1021
+#define IDI_SHIELD                      1022
 #define ID_SHELLNOTIFY                  4242
 
 // Next default values for new objects
@@ -61,7 +64,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        209
 #define _APS_NEXT_COMMAND_VALUE         32773
-#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_CONTROL_VALUE         1023
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
